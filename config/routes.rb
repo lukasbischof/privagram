a1183cc23182d50a71f7d9a7ba1bc1b8be11e0a3
Lukas Bischof <polinderis@gmail.com>
2017-08-14 10:43:33 +0200

    Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'home/index'
-    root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  root 'home#index'
 
-    get 'login', to: 'sessions#login'
+  get 'login', to: 'sessions#login'
 
-    get 'users/profile'
-    get '/users/:id/mypictures', to: 'users#mypictures'
+  get 'users/profile'
+  get '/users/:id/mypictures', to: 'users#mypictures'
 
-    get 'pictures/new'
+  get 'pictures/new'
 
-    resources :users do
-        resources :pictures do
-            resources :comments
-            resources :votes
-        end
+  resources :users do
+    resources :pictures do
+      resources :comments
+      resources :votes
     end
-    resources :sessions
+  end
+  resources :sessions
 end
